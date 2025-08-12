492c0a2fe3aa9db70766a422afb6dc228f5570e2
Justyna Gręda <83274413+gredajustyna@users.noreply.github.com>
2025-08-12 14:32:05 +0200

    Fix ESLint and Prettier configurations

---
+++ eslint.config.mjs
@@ -0,0 +1,35 @@
+import path from "path";
+import { fileURLToPath } from "url";
+import babelParser from "@babel/eslint-parser";
+import { fixupConfigRules } from "@eslint/compat";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const fileName = fileURLToPath(import.meta.url);
+const dir = path.dirname(fileName);
+
+const compat = new FlatCompat({
+  baseDirectory: dir,
+});
+
+/** @type {import('@types/eslint').Linter.Config[]} */
+const config = [
+  {
+    ignores: [".expo/", "expo-env.d.ts"],
+  },
+  {
+    languageOptions: {
+      parser: babelParser,
+    },
+  },
+  ...fixupConfigRules(compat.extends("@react-native/eslint-config")),
+  {
+    rules: {
+      "react/react-in-jsx-scope": ["off"],
+      "react/jsx-uses-react": ["off"],
+      "no-console": ["error", { allow: ["warn", "info", "error"] }],
+      "@typescript-eslint/func-call-spacing": "off",
+      "react-native/no-inline-styles": ["off"],
+    },
+  },
+];
+export default config;
