02dabe0e9fc14c045fc7acffa6ce8b44e5b0549a
Justyna Gręda <83274413+gredajustyna@users.noreply.github.com>
2025-08-12 13:45:17 +0200

    Import svg icons

---
+++ components/CustomIcon.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import ShelfSvg from "../assets/icons/bookshelf.svg";
+import RoutineSvg from "../assets/icons/face-woman-shimmer-outline.svg";
+import HomeSvg from "../assets/icons/home-sharp.svg";
+import ProfileSvg from "../assets/icons/person-circle-outline.svg";
+import SearchSvg from "../assets/icons/search-sharp.svg";
+import { SvgProps } from "react-native-svg";
+import { ViewStyle } from "react-native";
+
+export enum IconName {
+  Home,
+  Search,
+  Routine,
+  Shelf,
+  Profile,
+}
+
+interface StyledIconProps extends SvgProps {
+  color?: string;
+  name: IconName;
+}
+
+export const StyledIcon = ({ name, color, ...props }: StyledIconProps) => {
+  const iconMap = {
+    [IconName.Home]: HomeSvg,
+    [IconName.Search]: SearchSvg,
+    [IconName.Routine]: RoutineSvg,
+    [IconName.Shelf]: ShelfSvg,
+    [IconName.Profile]: ProfileSvg,
+  };
+
+  const IconComponent = iconMap[name];
+  return (
+    <IconComponent
+      {...props}
+      fill={color}
+      style={{ color: color } as ViewStyle}
+    />
+  );
+};
