062b42b589c2da890471eb6ab92cadbac0b8748b
Justyna Gręda <83274413+gredajustyna@users.noreply.github.com>
2025-08-13 13:43:28 +0200

    Create basic user data (#5)

--- mocks/routineData.ts
+++ mocks/routineData.ts
@@ -3,7 +3,7 @@ import databaseRaw from '../data/database.json';
 
 const database: Cosmetic[] = databaseRaw as Cosmetic[];
 
-export const Routine: Cosmetic[] = [
+export const ROUTINE: Cosmetic[] = [
   database[0],
   database[1267],
   database[2345],
@@ -12,4 +12,26 @@ export const Routine: Cosmetic[] = [
   database[5678],
   database[6789],
   database[7890],
+];
+
+export const SHELF: Cosmetic[] = [
+  database[1234],
+  database[2345],
+  database[3456],
+  database[4567],
+  database[5678],
+  database[6789],
+  database[7890],
+  database[8901],
+  database[0],
+  database[1267],
+  database[2348],
+  database[3459],
+  database[4560],
+  database[5671],
+  database[6782],
+  database[7893],
+  database[8904],
+  database[9015],
+  database[1236],
 ];
